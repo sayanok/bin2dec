c131b2ebf8cc791b63e02ab1220c1dea1a899b2f
sayanok <rng.jhn.osca@gmail.com>
2022-12-25 14:53:00 +0900

    add form add errorMessage

--- bin2dec/src/App.tsx
+++ bin2dec/src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [inputNumber, setInputNumber] = useState<number>();
+  const [convertedNumber, setConvertedNumber] = useState<number>();
+  const [digitErrorMessage, setDigitErrorMessage] = useState<string>();
+  const [numericErrorMessage, setNumericErrorMessage] = useState<string>();
+
+  function convert(inputNumber: number) {
+    return convertedNumber;
+  }
+
+  function checkInputNumber(inputNumber: string) {
+    if (inputNumber.length > 8) {
+      setDigitErrorMessage("8桁までしか入力できないよ");
+    } else {
+      setDigitErrorMessage("");
+    }
+    if (
+      inputNumber.includes("2") ||
+      inputNumber.includes("3") ||
+      inputNumber.includes("4") ||
+      inputNumber.includes("5") ||
+      inputNumber.includes("6") ||
+      inputNumber.includes("7") ||
+      inputNumber.includes("8") ||
+      inputNumber.includes("9")
+    ) {
+      setNumericErrorMessage("0か1しか入力できないよ");
+    } else {
+      setNumericErrorMessage("");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>数字をいれてね</label>
+        <input type="number" onChange={(e) => checkInputNumber(e.target.value)} />
+      </form>
+      {/* <p>{inputNumber}</p> */}
+      <p>{digitErrorMessage}</p>
+      <p>{numericErrorMessage}</p>
+      <p>10進数だと{convertedNumber}だよ</p>
     </div>
   );
-}
+};
 
 export default App;
