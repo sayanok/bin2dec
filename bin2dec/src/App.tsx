e389d42fd0256bccdc6d89deadbe2357d17d5e17
sayanok <rng.jhn.osca@gmail.com>
2022-12-25 16:09:41 +0900

    modify errorMessage 要件にあってないところを修正

--- bin2dec/src/App.tsx
+++ bin2dec/src/App.tsx
@@ -2,31 +2,20 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App: React.FC = () => {
-  const [inputNumber, setInputNumber] = useState<number>();
   const [convertedNumber, setConvertedNumber] = useState<number>();
-  const [digitErrorMessage, setDigitErrorMessage] = useState<string>();
   const [numericErrorMessage, setNumericErrorMessage] = useState<string>();
+  const regexp = new RegExp("[^0-1]");
 
-  function convert(inputNumber: number) {
-    return convertedNumber;
+  function convertToBinary(inputNumber: number): void {
+    let hoge: number;
+
+    hoge = 10;
+
+    setConvertedNumber(hoge);
   }
 
-  function checkInputNumber(inputNumber: string) {
-    if (inputNumber.length > 8) {
-      setDigitErrorMessage("8桁までしか入力できないよ");
-    } else {
-      setDigitErrorMessage("");
-    }
-    if (
-      inputNumber.includes("2") ||
-      inputNumber.includes("3") ||
-      inputNumber.includes("4") ||
-      inputNumber.includes("5") ||
-      inputNumber.includes("6") ||
-      inputNumber.includes("7") ||
-      inputNumber.includes("8") ||
-      inputNumber.includes("9")
-    ) {
+  function checkInputNumber(inputNumber: string): void {
+    if (regexp.exec(inputNumber)) {
       setNumericErrorMessage("0か1しか入力できないよ");
     } else {
       setNumericErrorMessage("");
@@ -37,10 +26,12 @@ const App: React.FC = () => {
     <div className="App">
       <form>
         <label>数字をいれてね</label>
-        <input type="number" onChange={(e) => checkInputNumber(e.target.value)} />
+        <textarea
+          maxLength={8}
+          rows={1}
+          onChange={(e) => [checkInputNumber(e.target.value), convertToBinary(parseInt(e.target.value))]}
+        />
       </form>
-      {/* <p>{inputNumber}</p> */}
-      <p>{digitErrorMessage}</p>
       <p>{numericErrorMessage}</p>
       <p>10進数だと{convertedNumber}だよ</p>
     </div>
