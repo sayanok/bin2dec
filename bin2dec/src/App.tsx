d7b3f5e1e0c8904bc5b7279c9ba5a29f1cedd038
sayanok <rng.jhn.osca@gmail.com>
2022-12-26 16:16:54 +0900

    fix

--- bin2dec/src/App.tsx
+++ bin2dec/src/App.tsx
@@ -1,29 +1,34 @@
 import React, { useState } from "react";
 
 const App: React.FC = () => {
-  const [convertedNumber, setConvertedNumber] = useState<number>(0);
+  const [convertedNumber, setConvertedNumber] = useState<number | string>("数字を入力してね");
   const [numericErrorMessage, setNumericErrorMessage] = useState<string>();
   const regexp = new RegExp("[^0-1]");
 
-  function convertToBinary(inputNumber: string): void {
-    if (!inputNumber) {
-      setConvertedNumber(0);
+  function onChangeHandler(inputNumber: string): void {
+    if (!checkInputNumber(inputNumber)) {
+      setConvertedNumber("不正な値が入力されています");
     } else {
-      let result = 0;
-      let n = 0;
-      for (let i = 0; i < inputNumber.length; i++) {
-        n = parseInt(inputNumber.charAt(i)) * 2 ** (inputNumber.length - (i + 1));
-        result = result + n;
-      }
-      setConvertedNumber(result);
+      setConvertedNumber(convertToBinary(inputNumber));
     }
   }
 
-  function checkInputNumber(inputNumber: string): void {
+  function convertToBinary(inputNumber: string): number {
+    let result = 0;
+    for (let i = 0; i < inputNumber.length; i++) {
+      const n = parseInt(inputNumber.charAt(i)) * 2 ** (inputNumber.length - (i + 1));
+      result = result + n;
+    }
+    return result;
+  }
+
+  function checkInputNumber(inputNumber: string): boolean {
     if (regexp.exec(inputNumber)) {
       setNumericErrorMessage("0か1しか入力できないよ");
+      return false;
     } else {
       setNumericErrorMessage("");
+      return true;
     }
   }
 
@@ -31,14 +36,10 @@ const App: React.FC = () => {
     <div className="App">
       <form>
         <label>数字をいれてね</label>
-        <textarea
-          maxLength={8}
-          rows={1}
-          onChange={(e) => [checkInputNumber(e.target.value), convertToBinary(e.target.value)]}
-        />
+        <textarea maxLength={8} rows={1} onChange={(e) => onChangeHandler(e.target.value)} />
       </form>
       <p>{numericErrorMessage}</p>
-      <p>10進数だと{convertedNumber}だよ</p>
+      <p>10進数だと「{convertedNumber}」</p>
     </div>
   );
 };
